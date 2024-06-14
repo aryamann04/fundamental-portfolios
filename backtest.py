38ae332ef21cabe2ccf1570b709c5ff5a06ba3dd
aryamann04 <140534650+aryamann04@users.noreply.github.com>
2024-06-14 17:36:21 +0900

    add russell 200 functionality to backtest.py

--- backtest.py
+++ backtest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
 
-from datapipeline import nasdaq_data, price
+from datapipeline import historical_data, price
 
 #------------------[rank_by]-----------------#
 #                                            #
@@ -12,8 +12,8 @@ from datapipeline import nasdaq_data, price
 #                                            #
 #--------------------------------------------#
 
-def rank_by(given_date, metric, **kwargs):
-    df = nasdaq_data(given_date)
+def rank_by(given_date, index, metric, **kwargs):
+    df = historical_data(given_date, index)
     metrics = ['CAPEI', 'bm', 'evm', 'pe_op_basic', 'pe_op_dil', 'pe_exi', 'pe_inc', 'ps', 'pcf', 'dpr', 'npm',
                'opmbd', 'opmad', 'gpm', 'ptpm', 'cfm', 'roa', 'roe', 'roce', 'efftax', 'aftret_eq', 'aftret_invcapx',
                'aftret_equity', 'pretret_noa', 'pretret_earnat', 'GProf', 'equity_invcap', 'debt_invcap',
@@ -45,8 +45,8 @@ def rank_by(given_date, metric, **kwargs):
 #                                            #
 #--------------------------------------------#
 
-def year_backtest(metric, start_date):
-    less_equal_zero, quintiles = rank_by(start_date, metric)
+def year_backtest(metric, start_date, index):
+    less_equal_zero, quintiles = rank_by(start_date, index, metric)
     portfolios = [less_equal_zero] + quintiles
     portfolio_tickers = [portfolio['TICKER'].tolist() for portfolio in portfolios]
 
@@ -63,7 +63,7 @@ def year_backtest(metric, start_date):
         price_data = pd.DataFrame()
 
         for ticker in tickers:
-            ticker_data = price(ticker, start_date, end_date)
+            ticker_data = price(ticker, start_date, end_date, index)
             if ticker_data is not None:
                 price_data[ticker] = ticker_data['prccd']
 
@@ -85,7 +85,7 @@ def year_backtest(metric, start_date):
 #                                            #
 #--------------------------------------------#
 
-def rebalanced_portfolio(metric, start_date='2000-06-30', end_date='2023-12-31'):
+def rebalanced_portfolio(metric, index, start_date='2000-06-30', end_date='2023-12-31'):
     current_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
 
@@ -93,7 +93,7 @@ def rebalanced_portfolio(metric, start_date='2000-06-30', end_date='2023-12-31')
     portfolio_stats = {i: [] for i in range(6)}
 
     while current_date < end_date:
-        annual_returns, portfolios = year_backtest(metric, current_date.strftime('%Y-%m-%d'))
+        annual_returns, portfolios = year_backtest(metric, current_date.strftime('%Y-%m-%d'), index)
 
         for i in range(6):
             cumulative_portfolios[i] = pd.concat([cumulative_portfolios[i], annual_returns[i]]).reset_index(drop=True)
